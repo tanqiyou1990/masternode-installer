1f42565e4da20b64e92cd74ab12921863c1b9408
Tan <tanqiyou@gmail.com>
2019-01-20 22:38:11 +0800

    update RPC

--- src/renderer/components/LandingPage/ZeroStep.vue
+++ src/renderer/components/LandingPage/ZeroStep.vue
@@ -15,7 +15,8 @@ const Client = require('motion-core');
 const client = new Client({
   username: 'mn',
   password: '999000',
-  port: 3385,
+  port: 9902,
+  // port: 3385,
 });
 
 export default {
